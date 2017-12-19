479357202e6ba1f68cc649403b22034e9fd9b282
Amit <amitskulkarni1@gmail.com>
2017-12-19 17:36:36 +0530

    DFS and BFS Java

--- Java/DsAlgo/GraphApp.java
+++ Java/DsAlgo/GraphApp.java
@@ -1,5 +1,36 @@
 import java.util.*;
 
+class QueueX {
+	int size = 30;
+	int[] qArr;
+	int front;
+	int rear;
+
+	QueueX() {
+		qArr = new int[size];
+		front = 0;
+		rear = -1;
+	}
+
+	public void insert(int newVal) {
+		if (rear == size - 1)
+			rear = -1;
+		qArr[++rear] = newVal;
+	}
+
+	public int remove() {
+		int temp = qArr[front++];
+		if (front == size)
+			front = 0;
+		return temp;
+	}
+
+	public boolean isEmpty() {
+		return (rear + 1 == front || front + size - 1 == rear);
+	}
+
+}
+
 class StackX {
 	int size = 30;
 	int[] st;
@@ -44,17 +75,17 @@ class Graph {
 	int adjMat[][];
 	int nVerts;
 	StackX thestack;
+	QueueX thequeue;
 
 	Graph() {
 		vertexList = new Vertex[max];
 		adjMat = new int[max][max];
 		nVerts = 0;
-		for (int j = 0; j < max; j++) {
-			for (int k = 0; k < max; k++) {
+		for (int j = 0; j < max; j++)
+			for (int k = 0; k < max; k++)
 				adjMat[j][k] = 0;
-				thestack = new StackX();
-			}
-		}
+		thestack = new StackX();
+		thequeue = new QueueX();
 	}
 
 	public void addVertex(char lab) {
@@ -93,6 +124,25 @@ class Graph {
 			if (adjMat[v][j] == 1 && vertexList[j].wasvisited == false)
 				return j;
 		return -1;
+	}
+
+	public void bfs() {
+		vertexList[0].wasvisited = true;
+		displayVertex(0);
+		thequeue.insert(0);
+		int v2;
+		while (!thequeue.isEmpty()) {
+			int v1 = thequeue.remove();
+			while ((v2 = getAdjUnvisitedVertex(v1)) != -1) {
+				vertexList[v2].wasvisited = true;
+				displayVertex(v2);
+				thequeue.insert(v2);
+			}
+		}
+		for (int j = 0; j < nVerts; j++) {
+			vertexList[j].wasvisited = false;
+
+		}
 	}
 
 }
