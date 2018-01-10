f63126a8ec6b2b1da072f2f2518ab9616578d9af
Amit <amitskulkarni1@gmail.com>
2018-01-10 09:41:28 +0530

    Function0.java

---
+++ Java/FunctionalJava/functions/Function0.java
@@ -0,0 +1,27 @@
+package functions;
+
+import java.util.concurrent.Callable;
+
+public abstract class Function0<R> implements Callable<R> {
+	private R value = null;
+
+	/**
+	 * The get() method should be synchronized, to ensure that we don't call
+	 * evaluate unnecessarily in a multithreaded setting.
+	 *
+	 * @return the result of evaluating -- results will be cached
+	 */
+	public final synchronized R get() {
+		if (value == null) {
+			value = evaluate();
+		}
+		return value;
+	}
+
+	public abstract R evaluate();
+
+	@Override
+	public R call() throws Exception {
+		return get();
+	}
+}
