b684da4ac2035331e88d80d29807eef6abf284c5
Akulkarni1 <amitskulkarni1@gmail.com>
2019-01-22 17:40:24 +0530

    Update SwapNumberApp.java

--- Java/withoutPomAndWithoutTest/DsAlgo/Bits/SwapNumberApp.java
+++ Java/withoutPomAndWithoutTest/DsAlgo/Bits/SwapNumberApp.java
@@ -11,6 +11,10 @@ public class SwapNumberApp {
 		System.out.println(a+" "+b);
 		
 		System.out.println(numberSame(50,150));
+		
+		System.out.println(Integer.toBinaryString(32));
+		
+		bitPattern(32);
 	}
 	public static boolean numberSame(int a,int b) {
 		
@@ -20,4 +24,30 @@ public class SwapNumberApp {
 	    else 
 	        return true; 
 	}
+	static void bitPattern(int n) {
+
+        int mask = 1 << 31;
+        int count = 0;
+        while(mask != 0) {
+            if(count%4 == 0)
+                System.out.print(" ");
+
+            if((mask&n) == 0) 
+
+                System.out.print("0");
+
+
+
+            else 
+                System.out.print("1");
+
+
+            count++;
+            mask = mask >>> 1;
+
+
+    }
+    System.out.println();
+}
+	
 }
