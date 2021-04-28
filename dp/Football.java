0ddbf437ee3eaf12bc648eef5f773d063c556e8e
Akulkarni1 <amitskulkarni1@gmail.com>
2021-04-28 19:25:23 +0530

    Create Football.java
    DP

---
+++ dp/Football.java
@@ -0,0 +1,19 @@
+class Football{
+    public static void main(String[] args){}
+
+    //Count the number of score combination in Football
+     public static int numCombinationForFinalScore(int finalScore, List<Integer> individualPlayScore){
+        int [][] numCombinationForFinalScore = new int [individualPlayScore.size()][finalScore+1];
+        for(int i=0;i<individualPlayScore.size();i++){
+            numCombinationForFinalScore[i][0]= 1;
+            for(int j=i;j<=finalScore;++j){
+                int withoutThisPlay = i-1>=0?numCombinationForFinalScore[i-1][j]:0;
+                int withThisPlay = j>=individualPlayScore.get(i)?numCombinationForFinalScore[i][j-individualPlayScore.get(i)]:0;
+                numCombinationForScore[i][j] = withThisPlay+withoutThisPlay;
+            }
+        }
+        return numCombinationForScore[individualPlayScore.size()-1][finalScore];
+    }
+//Time  Complexity of the above the program is O(sn) and the space complexity is O(sn)
+
+}
