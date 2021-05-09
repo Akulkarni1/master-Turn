e3ca6826a8d1d6f207ba633f06431949fa3496f6
Akulkarni1 <amitskulkarni1@gmail.com>
2021-05-09 23:08:09 +0530

    RenderingJsonInReact

---
+++ reactJs/sampleReact/helloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SampleHelloWorld from './sampleHelloWorld.jsx';
+
+ReactDOM.render(<SampleHelloWorld/>, document.getElementById(
+    'SampleHelloWordApp'
+));
