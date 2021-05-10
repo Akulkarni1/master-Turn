70845c5522ee7c6b325ff0f4097f8e0c6a4e08f4
Akulkarni1 <amitskulkarni1@gmail.com>
2021-05-10 22:57:02 +0530

    Attributes with React

---
+++ reactJs/sampleReact/myAttributeEx.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+//use of data helps us to create the temp local variable.
+class MyAttributeEx extends React.Component{
+    render() {
+        <div>
+            <h1>Header</h1>
+            <p data-myAttribute = "Value">Content of the attribute</p>
+        </div>
+    }
+}
