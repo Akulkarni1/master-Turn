a93ff451a47e217b2acfb38281359fd5caba0cf2
Akulkarni1 <amitskulkarni1@gmail.com>
2021-05-12 22:27:13 +0530

    reactJs props validation sample

---
+++ reactJs/sampleReact/propsMain.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './PropsApp.jsx';
+import PropsApp from "./PropsApp.jsx";
+
+ReactDOM.render(<PropsApp/>, document.getElementById('propApp'));
