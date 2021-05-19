2307055125f76dde3554a174e303fcfced945e40
Akulkarni1 <amitskulkarni1@gmail.com>
2021-05-19 22:22:29 +0530

    Create UpdateState

---
+++ reactJs/sampleReact/UpdateState.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class UpdateState extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {data: 'Intital data...'}
+        this.updateState = this.updateState.bind(this);
+    };
+
+    updateState(e) {
+        this.setState({data: e.target.value});
+    }
+
+    render() {
+        return (<div><Content myDataProp={this.state.data}
+                              updateStateProp={this.updateState}>
+        </Content>
+        </div>);
+    }
+}
+class Content extends React.Component{
+    render() {return(<div>
+        <input type = "text" value ={this.props.myDataProp}
+        onChange= ={this.props.updateStateProp}/>
+        <h3>this.props.myDataProp</h3>
+    </div>)
+    }
+}
