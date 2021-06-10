0db98dfc67b25e5c064d90b5bafccedef863c3f6
Akulkarni1 <amitskulkarni1@gmail.com>
2021-06-10 20:02:31 +0530

    Create LoggingButton.jsx

---
+++ reactJs/sampleReact/LoggingButton.jsx
@@ -0,0 +1,10 @@
+class LoggingButton extends React.Component{
+    handleClick(){
+        console.log("this is", this);
+    }
+    render() {
+        return(<button onClick={()=>this.handleClick()}>
+            Click me
+        </button> )
+    }
+}
